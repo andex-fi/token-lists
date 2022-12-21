ef37e7f5910ebebff74d0db59a112d2451886eb4
Nate <89227631+natedotv2@users.noreply.github.com>
2022-12-21 04:25:01 +0100

    Added Tokens

---
+++ sui/devnet.js
@@ -0,0 +1,55 @@
+[
+    {
+      address: '0x2::sui::SUI',
+      decimals: 9,
+      symbol: 'SUI',
+      name: 'Sui',
+      logoURL: 'https://tokenlist.animeswap.org/icons/APT.svg',
+      projectURL: 'https://sui.io/',
+    },
+    {
+      address: '0x100cbbf8632ab38788a3996be4717c385c43d54f::coins::USDT',
+      decimals: 8,
+      symbol: 'USDT',
+      name: 'Tether USD',
+      logoURL: 'https://tokenlist.andex.finance/icons/USDT.webp',
+      extensions: {
+        stableUSD: true,
+      },
+    },
+    {
+      address: '0x100cbbf8632ab38788a3996be4717c385c43d54f::coins::BTC',
+      decimals: 8,
+      symbol: 'BTC',
+      name: 'Bitcoin',
+      logoURL: 'https://tokenlist.andex.finance/icons/BTC.webp',
+    },
+    {
+      address: '0x100cbbf8632ab38788a3996be4717c385c43d54f::coins::DAI',
+      decimals: 8,
+      symbol: 'DAI',
+      name: 'Dai Stablecoin',
+      logoURL: 'https://tokenlist.andex.finance/icons/DAI.webp',
+      extensions: {
+        stableUSD: true,
+      },
+    },
+    {
+      address: '0x100cbbf8632ab38788a3996be4717c385c43d54f::coins::USDC',
+      decimals: 8,
+      symbol: 'USDC',
+      name: 'USD Coin',
+      logoURL: 'https://tokenlist.andex.finance/icons/USDC.webp',
+      extensions: {
+        stableUSD: true,
+      },
+    },
+    {
+      address: '0x100cbbf8632ab38788a3996be4717c385c43d54f::coins::ETH',
+      decimals: 8,
+      symbol: 'ETH',
+      name: 'Wrapped Ethereum',
+      logoURL: 'https://tokenlist.andex.finance/icons/WETH.png',
+    },
+  ]
+  
